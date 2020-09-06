a492cab4f4e04b0a51039d2bdf5018c4769c5dff
mjrgeorge <mjrgeorge@gmail.com>
2020-09-06 22:12:46 +0600

    Not Match style update

--- src/components/notMatch/NotMatch.js
+++ src/components/notMatch/NotMatch.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
 
 const NotMatch = () => {
     return (
-        <div>
-            <h1>Sorry, Page Not Found</h1>
-        </div>
+        <Card style={{margin: '10%', textAlign: 'center'}}>
+            <CardContent>
+                <Typography gutterBottom variant="h1" component="div">404 Error</Typography>
+                <Typography variant="body1" color="textSecondary" component="div"><h1>Sorry, Page Not Found</h1></Typography>
+            </CardContent>
+        </Card>
     );
 };
 
